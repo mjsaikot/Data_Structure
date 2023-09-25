e77b0558fa713c3a4311cb9adcccd991fc6a7367
mjsaikot <mjsaikot@gmail.com>
2023-09-25 23:54:57 +0600

    add file

--- Module_6/Insert_at_any_position.cpp
+++ Module_6/Insert_at_any_position.cpp
@@ -13,26 +13,26 @@ class Node
         this->next = NULL;
     }
 };
+
 void insert_at_tail(Node* &head, int v)
 {
-    Node* newNode =new Node(v);
+    Node* newNode = new Node(v);
     if(head == NULL)
     {
-        head = newNode;
+        newNode = head;
         return;
     }
-
     Node* tmp = head;
     while(tmp->next != NULL)
     {
         tmp = tmp->next;
     }
-    //after finish the loop tmp will stay in last node;
     tmp->next = newNode;
 }
 void print_linked_list(Node* head)
 {
     Node* tmp = head;
+    cout<<"Your Linked list is: ";
     while(tmp != NULL)
     {
         cout<<tmp->val<<" ";
@@ -40,16 +40,15 @@ void print_linked_list(Node* head)
     }
     cout<<endl<<endl;
 }
-
 int main() {
     Node* head = NULL;
     while(true)
     {
-        cout<<"Option 1: Insert at Tail"<<endl;
-        cout<<"Option 2: Print the linked list"<<endl;
-        cout<<"Option 3: Terminate Program"<<endl;
-        int op;
-        cin>>op;
+        cout<<"Option 1: Enter at tail."<<endl;
+        cout<<"Option 2: Enter at any position."<<endl;
+        cout<<"Option 3: Print your linked list."<<endl;
+        cout<<"Option 4: Terminate your program."<<endl;
+        int op; cin>>op;
         if(op == 1)
         {
             int v;
@@ -59,14 +58,16 @@ int main() {
         }
         else if(op == 2)
         {
-            cout<<"Your Linked list is: ";
-            //call print function;
+
+        }
+        else if(op == 3)
+        {
             print_linked_list(head);
         }
         else{
             break;
         }
-        
-    }   
+    }
+   
    return 0;
 }
