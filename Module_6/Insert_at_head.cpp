7a25e94ff83ff391440c5f0f7a1c35e9dd7f8412
mjsaikot <mjsaikot@gmail.com>
2023-09-29 02:01:20 +0600

    Add file

--- Module_6/Insert_at_head.cpp
+++ Module_6/Insert_at_head.cpp
@@ -5,12 +5,12 @@ class Node
 {
     public:
         int val;
-        Node* next;
+        Node *next;
 
-    Node(int val)
-    {
-        this->val = val;
-        this->next = NULL;
+        Node(int val)
+        {
+            this->val = val;
+            this->next = NULL;
     }
 };
 void insert_at_tail(Node* &head, int v)
@@ -41,7 +41,28 @@ void print_linked_list(Node* head)
     cout << endl
          << endl;
 }
-int main() {
+void insert_any_pos(Node *head, int pos, int val)
+{
+    Node *newNode = new Node(val);
+    Node *tmp = head;
+    for (int i = 1; i <= pos - 1; i++)
+    {
+        tmp = tmp->next;
+    }
+    // Linked with pos-1 with new Node;
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+}
+void insert_at_head(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+    cout << endl
+         << "Inerted at head" << endl;
+}
+int main()
+{
     Node *head = NULL;
     while(true)
     {
@@ -66,7 +87,29 @@ int main() {
             cout << endl
                  << endl;
         }
-        else{break;}
+        else if(op == 3)
+        {
+            int pos, val;
+            cout << "Enter your position here: ";
+            cin >> pos;
+            cout << "Enter your value: ";
+            cin >> val;
+            if(pos == 0)
+            {
+                insert_at_head(head, val);
+            }
+            else{
+                insert_any_pos(head, pos, val);
+            }
+            
+            cout << endl
+                 << endl;
+            
+        }
+        else
+        {
+            break;
+        }
     }
 
     return 0;
