9ca0bb5c50795589522bfd3bd3b27e7424e665c8
mjsaikot <mjsaikot@gmail.com>
2023-09-30 20:06:50 +0600

    update file

--- Module_6/Delete_Node.cpp
+++ Module_6/Delete_Node.cpp
@@ -60,16 +60,27 @@ void insert_any_pos(Node *head, int pos, int val)
     newNode->next = tmp->next;
     tmp->next = newNode;
 }
+void delete_node(Node* head, int pos)
+{
+    Node *tmp = head;
+    for (int i = 1; i <= pos - 1; i++)
+    {
+        tmp = tmp->next;
+    }
+    Node *deleteNode = tmp->next;
+    tmp->next = tmp->next->next;
+    delete deleteNode;
+}
 int main()
 {
     Node *head = NULL;
-    while (true)
+    while(true)
     {
         int op;
-        cout << "Option 1: Insert at Tail." << endl;
-		cout << "Option 2: Print the linked list." << endl;
-		cout << "Option 3: Enter at any position." << endl;
-		cout << "Option 4: Insert at Head." << endl;
+        cout << "Option 1: Insert at Head." << endl;
+		cout << "Option 2: Insert at any position." << endl;
+		cout << "Option 3: Insert at Tail." << endl;
+		cout << "Option 4: Print the linked list." << endl;
 		cout << "Option 5: Delete a Node." << endl;
 		cout << "Option 6: Terminate Program." << endl;
         cout << endl;
@@ -77,18 +88,12 @@ int main()
         cin >> op;
         if(op == 1)
         {
-            int v;
-            cout << "Enter value: ";
-            cin >> v;
-            insert_at_tail(head, v);
+            int val;
+            cout << "Enter Your value: ";
+            cin >> val;
+            insert_at_head(head, val);
         }
         else if(op == 2)
-        {
-            cout << endl;
-            cout << "Your Linked list is: ";
-            print_linked_list(head);
-        }
-        else if(op == 3)
         {
             int pos, val;
             cout << "Enter your position: ";
@@ -101,19 +106,36 @@ int main()
             }
             else{
                 insert_any_pos(head, pos, val);
-                cout << "Inserted " << val << "at " << pos << " no. position." << endl;
+                cout << "Inserted " << val << " at " << pos << " no. position." << endl;
             }
         }
-        else if(op == 4)
+        else if(op == 3)
         {
-            int val;
-            cout << "Enter Your value: ";
-            cin >> val;
-            insert_at_head(head, val);
+            int v;
+            cout << "Enter value: ";
+            cin >> v;
+            insert_at_tail(head, v);
         }
-        else{
+        else if(op = 4)
+        {
+            cout << endl;
+            cout << "Your Linked list is: ";
+            print_linked_list(head);
+        }
+        else if(op == 5)
+        {
+            int pos;
+            cout << "Enter Position for delete Node: ";
+            cin >> pos;
+            delete_node(head, pos);
+            cout << "Node Deleted" << endl;
+        }
+        else
+        {
+            // terminate program;
             break;
         }
-   }
-   return 0;
+    }
+
+    return 0;
 }
