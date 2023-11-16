f2bc5b2327679467c7378120f04d650f92b7ce79
mjsaikot <mjsaikot@gmail.com>
2023-11-16 21:45:19 +0600

    Modify file

---
+++ Assignment_Day_2/ques_5.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void insert_at_head(Node *&head, Node *&tail, int val)
+{
+}
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+}
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+
+    return 0;
+}
