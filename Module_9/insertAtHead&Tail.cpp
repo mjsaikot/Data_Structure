c871ca3fdc38250e804eb9b73437d889724091c8
mjsaikot <mjsaikot@gmail.com>
2023-11-24 03:15:57 +0600

    add file

--- Module_9/insertAtHead&Tail.cpp
+++ Module_9/insertAtHead&Tail.cpp
@@ -28,9 +28,25 @@ void insert_at_tail(Node *&head, Node *&tail, int val)
     tail->next = newNode;
     tail = newNode;
 }
+void insert_at_head(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+    newNode->next = head;
+    head->prev = newNode;
+    head = newNode;
+}
 void insertAtPos(Node *head, int pos, int val)
 {
     Node *newNode = new Node(val);
+    if (pos == 0)
+    {
+        insert_at_head(head, val);
+    }
     Node *tmp = head;
     for (int i = 1; i <= pos - 1; i++)
     {
@@ -63,25 +79,23 @@ void print_reverse(Node *tail)
 }
 int main()
 {
-    Node *head = nullptr;
-    Node *tail = nullptr;
+    Node *head = NULL;
+    Node *tail = NULL;
     int val;
     while (true)
     {
         cin >> val;
-        if (val == -1)
+        if (val = -1)
         {
             break;
         }
         insert_at_tail(head, tail, val);
     }
     printNormal(head);
-    print_reverse(tail);
     int pos;
     cin >> pos >> val;
     insertAtPos(head, pos, val);
     printNormal(head);
     print_reverse(tail);
-
     return 0;
 }
