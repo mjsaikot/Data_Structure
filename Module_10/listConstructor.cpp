2af02fbd1fd60e29f24b9864ac61f32c20681db1
mjsaikot <mjsaikot@gmail.com>
2023-11-29 02:44:15 +0600

    Add Module 10

---
+++ Module_10/listConstructor.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    list<int> myList = {10, 20, 30, 40, 50};
+    // cout << myList.size << endl;
+    for (int val : myList)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+    return 0;
+}
