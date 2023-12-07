29e124714d5cc12cfe5372d25db31715a8c383be
mjsaikot <mjsaikot@gmail.com>
2023-12-08 00:39:40 +0600

    add file

--- Module_10/listCapacity.cpp
+++ Module_10/listCapacity.cpp
@@ -1,9 +1,23 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+// here we use Linked list capacity function.
 int main()
 {
+    list<int> myList = {10, 20, 30};
+    cout << myList.max_size() << endl;
+    cout << myList.size() << endl;
+    for (int val : myList)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+    myList.resize(2);
+    for (int val : myList)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
 
     return 0;
 }
