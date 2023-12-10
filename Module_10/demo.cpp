d7b91f035141347fc80dedcca82cc52f391b1e5a
mjsaikot <mjsaikot@gmail.com>
2023-12-11 00:01:08 +0600

    Add file

---
+++ Module_10/demo.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+void insertAtHead(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == nullptr)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    newNode->next = head;
+    head->prev = newNode;
+    head = newNode;
+}
+void insertAtTail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if(head == nullptr)
+    {
+        head = newNode;
+    }
+}
+int main()
+{
+    return 0;
+}
