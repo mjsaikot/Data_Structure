760056a01484319912372f050ae16162963e433b
mjsaikot <mjsaikot@gmail.com>
2023-12-14 04:28:55 +0600

    Add file

--- Module_10/demo.cpp
+++ Module_10/demo.cpp
@@ -31,9 +31,70 @@ void insertAtHead(Node *&head, Node *&tail, int val)
 void insertAtTail(Node *&head, Node *&tail, int val)
 {
     Node *newNode = new Node(val);
-    if(head == nullptr)
+    if (head == nullptr)
     {
         head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+}
+void insertAtPos(Node *&head, Node *&tail, int pos, int val)
+{
+    if (pos == 0)
+    {
+        insertAtHead(head, tail, val);
+        return;
+    }
+    Node *newNode = new Node(val);
+    Node *tmp = head;
+    for (int i = 1; i <= pos - 1; i++)
+    {
+        tmp = tmp->next;
+    }
+    if (tmp == nullptr)
+    {
+        cout << "Invalid Position" << endl;
+        return;
+    }
+    newNode->next = tmp->next;
+    newNode->prev = tmp;
+    if (tmp->next != nullptr)
+    {
+        tmp->next->prev = newNode;
+    }
+    tmp->next = newNode;
+    if (newNode->next == nullptr)
+    {
+        tail = newNode;
+    }
+}
+void deleteNode(Node *&head, Node *&tail, int pos)
+{
+    if (head == nullptr || pos < 0)
+    {
+        cout << "Invalid position or empty list" << endl;
+        return;
+    }
+    Node *tmp = head;
+    for (int i = 0; tmp != nullptr && i < pos; i++)
+    {
+        tmp = tmp->next;
+    }
+    if (tmp == nullptr)
+    {
+        cout << "Invalid position" << endl;
+        return;
+    }
+    if (tmp == head)
+    {
+        head = tmp->next;
+    }
+    if (tmp == tail)
+    {
+        tail = tmp->prev;
     }
 }
 int main()
