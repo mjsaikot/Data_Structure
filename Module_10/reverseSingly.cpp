c62d08a9c55096b496da8cd269bb73485abc30f5
mjsaikot <mjsaikot@gmail.com>
2023-12-19 23:17:57 +0600

    Add file

---
+++ Module_10/reverseSingly.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void printNormal(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != nullptr)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+void reverse(Node *&head, Node *cur)
+{
+    if (cur->next == NULL)
+    {
+        head = cur;
+        return;
+    }
+    reverse(head, cur->next);
+    cur->next->next = cur;
+    cur->next = nullptr;
+}
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+
+    head->next = a;
+    a->next = b;
+
+    reverse(head, head);
+    printNormal(head);
+    return 0;
+}
