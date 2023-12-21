23884a02bdea6cf716672ef4c3b7fd739e65fc4c
mjsaikot <mjsaikot@gmail.com>
2023-12-22 00:03:15 +0600

    Add file

---
+++ Practice_day_4/Ques_1.cpp
@@ -0,0 +1,13 @@
+/*Take two doubly linked lists as input and check if they are the same or not.*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    list<int> myList1;
+    list<int> myList2;
+    for ()
+        return 0;
+}
