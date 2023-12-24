5b58029e51b86c4d42ecffcd804337401a5f5611
mjsaikot <mjsaikot@gmail.com>
2023-12-25 00:16:50 +0600

    Add file

--- Practice_day_4/Ques_1.cpp
+++ Practice_day_4/Ques_1.cpp
@@ -1,13 +1,102 @@
-/*Take two doubly linked lists as input and check if they are the same or not.*/
 #include <bits/stdc++.h>
 
 using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
 
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+void insertAtTail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == nullptr)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    newNode->prev = tail;
+    tail->next = newNode;
+    tail = newNode;
+}
+void printOut(Node *head1, Node *head2)
+{
+    Node *tmp1 = head1;
+    Node *tmp2 = head2;
+    while (tmp1 != nullptr)
+    {
+        cout << tmp1->val << " ";
+        tmp1 = tmp1->next;
+    }
+    cout << endl;
+    while (tmp2 != nullptr)
+    {
+        cout << tmp2->val << " ";
+        tmp2 = tmp2->next;
+    }
+    cout << endl;
+}
+bool findSame(Node *head1, Node *head2)
+{
+    if (head1 == nullptr && head2 == nullptr)
+    {
+        cout << "Linked List is empty" << endl;
+        // break;
+    }
+    while (head1 != nullptr && head2 != nullptr)
+    {
+        if (head1->val != head2->val)
+        {
+            return false;
+        }
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+    return head1 == nullptr && head2 == nullptr;
+}
 int main()
 {
+    Node *head1 = nullptr;
+    Node *tail1 = nullptr;
+    Node *head2 = nullptr;
+    Node *tail2 = nullptr;
+    int val;
 
-    list<int> myList1;
-    list<int> myList2;
-    for ()
-        return 0;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insertAtTail(head1, tail1, val);
+    }
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insertAtTail(head2, tail2, val);
+    }
+    if (findSame(head1, head2))
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
 }
